a848da0b761de8698c62bd82111b2c8a4e6db685
Michał Szwejk <mszwejk1@gmail.com>
2025-12-11 14:17:15 +0100

    change selector signature

--- src/selector/selector.py
+++ src/selector/selector.py
@@ -6,21 +6,22 @@ from src.model.classifier import Classifier
 
 
 class Selector(Protocol):
-    def __call__(
-        self, classifier: Classifier, X: np.ndarray, batch_size: int = 5
-    ) -> np.ndarray: ...
+    def __call__(self, X: np.ndarray, batch_size: int = 5) -> np.ndarray: ...
 
 
 class UncertaintySelector:
-    def __call__(self, classifier: Classifier, X: np.ndarray, batch_size: int = 5) -> np.ndarray:
-        proba = classifier.predict_proba(X)
+    def __init__(self, classifier: Classifier) -> None:
+        self.classifier: Classifier = classifier
+
+    def __call__(self, X: np.ndarray, batch_size: int = 5) -> np.ndarray:
+        proba = self.classifier.predict_proba(X)
         max_proba = np.max(proba, axis=1)
 
         return np.argsort(np.abs(max_proba - 0.5))[:batch_size]
 
 
 class DiversitySelector:
-    def __call__(self, classifier: Classifier, X: np.ndarray, batch_size: int = 5) -> np.ndarray:
+    def __call__(self, X: np.ndarray, batch_size: int = 5) -> np.ndarray:
         distances = np.linalg.norm(X[:, np.newaxis, :] - X[np.newaxis, :, :], axis=2)
         mean_distance = np.mean(distances, axis=1)
 
