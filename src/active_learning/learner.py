ad94ff85b983a9689d50689a13ff63e8c880eb44
Michał Szwejk <mszwejk1@gmail.com>
2025-12-19 01:55:35 +0100

    implement offline learner

---
+++ src/active_learning/learner.py
@@ -0,0 +1,185 @@
+from concurrent.futures import ProcessPoolExecutor
+from dataclasses import dataclass
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import RepeatedStratifiedKFold
+
+from src.models.classifier import Classifier
+
+from .metrics import calculate_pr_metrics
+from .selector import Selector
+
+type AUCHistory = list[tuple[float, float]]
+type PRHistory = list[tuple[float, np.ndarray, np.ndarray]]
+
+
+@dataclass
+class ActiveLearnerConfig:
+    classifier: Classifier
+    selector: Selector
+    batch_size: int
+    store_metrics: bool
+
+
+@dataclass
+class LearningData:
+    X_train: np.ndarray
+    y_train: np.ndarray
+    X_unlabeled: np.ndarray
+    y_oracle: np.ndarray
+
+
+class ActiveLearner:
+    def __init__(self, config: ActiveLearnerConfig, data: LearningData) -> None:
+        self.classifier = config.classifier
+        self.selector = config.selector
+        self.batch_size = config.batch_size
+        self.store_metrics = config.store_metrics
+        self.data = data
+
+        if config.store_metrics:
+            self.thresholds: list[float] = [0.25, 0.35, 0.5, 0.75, 1.0]
+            self.auc: AUCHistory = []
+            self.pr: PRHistory = []
+
+    def loop(self, X_test: np.ndarray, y_test: np.ndarray) -> None:
+        self.classifier.fit(self.data.X_train, self.data.y_train)
+        while self.data.X_unlabeled.shape[0] != 0:
+            samples_indices = self.selector(
+                self.data.X_unlabeled, self.data.X_train, self.batch_size
+            )
+
+            for sample_index in sorted(samples_indices, reverse=True):
+                self._label_sample_using_oracle(sample_index)
+
+            self.classifier.fit(self.data.X_train, self.data.y_train)
+
+            if self.store_metrics:
+                self._store_metrics(X_test, y_test)
+
+    def _label_sample_using_oracle(self, sample_index: int) -> None:
+        sample = self.data.X_unlabeled[sample_index, :]
+        label = self.data.y_oracle[sample_index]
+
+        self.data.X_unlabeled = np.delete(self.data.X_unlabeled, sample_index, axis=0)
+        self.data.X_train = np.concatenate((self.data.X_train, sample[np.newaxis, :]), axis=0)
+
+        self.data.y_oracle = np.delete(self.data.y_oracle, sample_index)
+        self.data.y_train = np.append(self.data.y_train, int(label))
+
+    def _store_metrics(self, X_test: np.ndarray, y_test: np.ndarray) -> None:
+        precision, recall, auc = calculate_pr_metrics(self.classifier, X_test, y_test)
+        thr = self._get_closest_unsaved_threshold()
+        if thr is not None:
+            recall_grid = np.linspace(0, 100, 1)
+            self.pr.append(
+                (thr, np.interp(recall_grid, recall[::-1], precision[::-1]), recall_grid)
+            )
+        self.auc.append((self._get_labeled_ratio(), auc))
+
+    def _get_closest_unsaved_threshold(self) -> float | None:
+        labeled_ratio = self._get_labeled_ratio()
+        for thr in self.thresholds:
+            if labeled_ratio >= thr:
+                if not any([saved_thr == thr for saved_thr, _, _ in self.pr]):
+                    return thr
+            else:
+                break
+
+    def _get_labeled_ratio(self) -> float:
+        return self.data.X_train.shape[0] / (
+            self.data.X_train.shape[0] + self.data.X_unlabeled.shape[0]
+        )
+
+
+@dataclass
+class TesterConfig:
+    save_dir: Path
+    n_splits: int = 5
+    n_repeats: int = 3
+    unlabeled_ratio: float = 0.2
+    seed: int = 42
+
+
+class LearnerTester:
+    def __init__(self, learner_config: ActiveLearnerConfig, config: TesterConfig) -> None:
+        self.learner_config = learner_config
+        self.save_dir = config.save_dir
+        self.n_splits = config.n_splits
+        self.n_repeats = config.n_repeats
+        self.unlabeled_ratio = config.unlabeled_ratio
+        self.tester_rng = np.random.default_rng(config.seed)
+
+    def aggregate_results(self, X: np.ndarray, y: np.ndarray) -> None:
+        if not self.learner_config.store_metrics:
+            raise ValueError("Learener must store metrics to aggregate them later")
+
+        learners: list[ActiveLearner] = []
+        learners_X_test: list[np.ndarray] = []
+        learners_y_test: list[np.ndarray] = []
+
+        rskf = RepeatedStratifiedKFold(n_splits=self.n_splits, n_repeats=self.n_repeats)
+        for pool_index, test_index in rskf.split(X, y):
+            X_pool, y_pool = X[pool_index, :], y[pool_index]
+            X_test, y_test = X[test_index, :], y[test_index]
+
+            threshold = int(X_pool.shape[0] * self.unlabeled_ratio)
+            X_train, y_train = X_pool[threshold:, :], y_pool[threshold:]
+            X_unlabeled, y_oracle = X_pool[:threshold, :], y_pool[:threshold]
+
+            learner = ActiveLearner(
+                self.learner_config, LearningData(X_train, y_train, X_unlabeled, y_oracle)
+            )
+            learners.append(learner)
+            learners_X_test.append(X_test)
+            learners_y_test.append(y_test)
+
+        with ProcessPoolExecutor() as executor:
+            result = executor.map(
+                self._run_single_learner, learners, learners_X_test, learners_y_test
+            )
+            executor.shutdown()
+
+        all_auc: list[AUCHistory] = []
+        all_pr: list[PRHistory] = []
+        for auc, pr in result:
+            all_auc.append(auc)
+            all_pr.append(pr)
+
+        self.save_dir.mkdir(parents=True, exist_ok=True)
+        self._process_auc(all_auc)
+        self._process_pr(all_pr)
+
+    def _process_auc(self, all_auc: list[AUCHistory]) -> None:
+        labeled_ratio = np.array([t[0] for t in all_auc[0]])
+        values = np.array([[t[1] for t in record] for record in all_auc])
+
+        data = {
+            "labeled_ratio": labeled_ratio,
+            "mean": np.mean(values, axis=0),
+            "std": np.std(values, axis=0),
+            "min": np.min(values, axis=0),
+            "max": np.max(values, axis=0),
+        }
+
+        pd.DataFrame(data).to_csv(self.save_dir / "auc-results.csv")
+
+    def _process_pr(self, all_pr: list[PRHistory]) -> None:
+        thresholds = np.array([t[0] for t in all_pr[0]])
+        precision = np.array([[t[1] for t in record] for record in all_pr])
+        recall = np.array([[t[2] for t in record] for record in all_pr])
+
+        precision_mean = np.mean(precision, axis=0)
+        recall_mean = np.mean(recall, axis=0)
+
+        for i, thr in enumerate(thresholds):
+            data = {"recall": recall_mean[i, :], "precision": precision_mean[i, :]}
+            pd.DataFrame(data).to_csv(self.save_dir / f"pr-curve-{thr}.csv")
+
+    def _run_single_learner(
+        self, learner: ActiveLearner, X_test: np.ndarray, y_test: np.ndarray
+    ) -> tuple[AUCHistory, PRHistory]:
+        learner.loop(X_test, y_test)
+        return learner.auc, learner.pr
--- src/main.py
+++ src/main.py
@@ -5,49 +5,35 @@ import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from src.config import ConfigParser
-from src.config.logger import LoggerConfig
-from src.learner.learner import ActiveLearner, ActiveLearnerConfig, LearningData
-from src.model.classifier import resolve_model
-from src.selector.selector import resolve_selector
+from src.active_learning.learner import ActiveLearnerConfig, LearnerTester, TesterConfig
+from src.active_learning.selector import resolve_selector
+from src.config import ConfigParser, LoggerConfig
+from src.models.classifier import resolve_model
 from src.utils.logger import setup_logger
 
 
-def parse_config():
+def main():
     config_parser = ConfigParser()
     logger_config = config_parser.get(LoggerConfig)
     setup_logger(logger_config)
+    df = pd.read_csv("data/active_learning/flowers.csv")
 
-
-def main():
-    args = parse_args()
-
-    logger_config = get_logger_config_from_args(args)
-    _ = setup_logger(logger_config)
-
-    df = pd.read_csv("./data/wine-quality.csv")
-    x, y = df.iloc[:, :-2], df.iloc[:, -2:-1]
+    x, y = df.iloc[:, :-1], df.iloc[:, -1]
 
     x = x.to_numpy()
     y = y.to_numpy()
-
-    y = np.where(y <= 6, 1, 0).ravel()
 
     x_train, x_test, y_train, y_test = cast(
         tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray], train_test_split(x, y, test_size=0.8)
     )
 
-    classifier = resolve_model(args.model)
-    selector = resolve_selector(args.selector, classifier)
+    classifier = resolve_model("forest")
+    selector = resolve_selector("random", classifier)
 
-    learner_config = ActiveLearnerConfig(
-        classifier,
-        selector,
-        Path(args.session_name),
-        LearningData(x_train[:-10, :], y_train[:-10], x_train[-10:, :]),
-    )
-    learner = ActiveLearner(learner_config)
-    learner.loop(x_test, y_test)
+    learner_config = ActiveLearnerConfig(classifier, selector, 5, True)
+    tester_config = TesterConfig(Path("abc"), n_splits=2, n_repeats=1)
+    tester = LearnerTester(learner_config, tester_config)
+    print(tester.aggregate_results(x, y))
 
 
 if __name__ == "__main__":
