354b327114dcd055eb05d93da041e2fbe5fe0a06
Michał Szwejk <mszwejk1@gmail.com>
2025-12-23 19:00:42 +0100

    add progress_bar util

---
+++ src/utils/progress_bar.py
@@ -0,0 +1,12 @@
+from rich import progress
+
+
+def setup_progress_bars() -> progress.Progress:
+    return progress.Progress(
+        "[progress.description]{task.description}",
+        progress.BarColumn(),
+        "[progress.percentage]{task.percentage:>3.0f}%",
+        progress.TimeRemainingColumn(),
+        progress.TimeElapsedColumn(),
+        refresh_per_second=1,
+    )
