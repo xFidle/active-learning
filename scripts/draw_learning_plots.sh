de39450efc9092a95f4c54d58e71374fb20ae6b9
Michał Szwejk <mszwejk1@gmail.com>
2026-01-27 17:47:26 +0100

    move commands to different dir

--- scripts/draw_learning_plots.sh
+++ scripts/draw_learning_plots.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 for classifier in "svm" "forest"; do
   for initializer in "random" "cluster"; do
     echo "Drawing plot for "$classifier"_"$initializer""
-    uv run python -m src.plot_learning_curve "$classifier" "$initializer" 
+    uv run python -m src.commands.plot_learning_curve "$classifier" "$initializer" 
   done
 done
--- scripts/draw_pr_plots.sh
+++ scripts/draw_pr_plots.sh
@@ -5,7 +5,7 @@ for classifier in "svm" "forest"; do
   for initializer in "random" "cluster"; do
     for selector in "uncertainty" "diversity" "random"; do
       echo "Drawing plot for "$classifier"_"$initializer"_"$selector""
-      uv run python -m src.plot_pr_curve "$classifier" "$initializer" "$selector" --ths 25 30 50 100
+      uv run python -m src.commands.plot_pr_curve "$classifier" "$initializer" "$selector" --ths 25 30 50 100
     done
   done
 done
